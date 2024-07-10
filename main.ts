2c20fa5194c380e04382c6896c028c6f56e7971c
Tahirah Roohi <157677736+TahirahWebDev@users.noreply.github.com>
2024-07-10 01:11:06 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,66 @@
+// Question 25
+
+let alienColor = "green";
+if (alienColor == "green") {
+    console.log("You just earned 5 points!");
+};
+
+alienColor = "red";
+if (alienColor == "green") {
+    console.log("You just earned 5 points!"); // no output
+};
+
+
+// Question 26
+
+alienColor = "green";
+if (alienColor == "green") {
+    console.log("You just earned 5 points for shooting the alien!"); 
+} else {
+    console.log("You just earned  10 points")
+}
+
+alienColor = "yellow";
+if (alienColor == "green") {
+    console.log("You just earned 5 points for shooting the alien!"); 
+} else {
+    console.log("You just earned  10 points")
+}
+
+
+// Question 27
+
+alienColor = "green";
+if (alienColor == "green") {
+    console.log("You just earned 5 points!"); 
+}
+else if (alienColor == "yellow") {
+    console.log("You earned 10 points!")
+}
+else if (alienColor == "red") {
+    console.log("You just earned  15 points!")
+}
+
+
+alienColor = "yellow";
+if (alienColor == "green") {
+    console.log("You just earned 5 points!"); 
+}
+else if (alienColor == "yellow") {
+    console.log("You earned 10 points!")
+}
+else if (alienColor == "red") {
+    console.log("You just earned  15 points!")
+}
+
+
+alienColor = "red";
+if (alienColor == "green") {
+    console.log("You just earned 5 points!"); 
+}
+else if (alienColor == "yellow") {
+    console.log("You earned 10 points!")
+}
+else if (alienColor == "red") {
+    console.log("You just earned  15 points!")
+}
